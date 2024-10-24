bb5f7d5f2d6c6b67f69cc155b97fbed577470894
IlMeloIl <jgvmelo123@gmail.com>
2024-10-24 12:48:04 -0300

    Add extra validation CartaoCredito

--- scb/src/main/java/com/example/demo/model/CartaoCredito.java
+++ scb/src/main/java/com/example/demo/model/CartaoCredito.java
@@ -3,6 +3,7 @@ package com.example.demo.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
@@ -22,10 +23,19 @@ public class CartaoCredito {
     @JoinColumn(name = "ciclista_id")
     private Ciclista ciclista;
 
+    @NotBlank(message = "Número do cartão é obrigatório")
 	@Pattern(regexp = "^\\d{16}$", message = "Número do cartão deve ter 16 dígitos")
     private String numero;
+    
+    @NotBlank(message = "Nome do titular é obrigatório")
     private String nomeTitular;
+    
+    @NotBlank(message = "Validade é obrigatória")
+    @Pattern(regexp = "^(0[1-9]|1[0-2])/([0-9]{2})$", message = "Validade deve estar no formato MM/YY")
     private String validade;
+    
+    @NotBlank(message = "CVV é obrigatório")
+    @Pattern(regexp = "^[0-9]{3,4}$", message = "CVV deve ter 3 ou 4 dígitos")
     private String cvv;
     private boolean principal = false;
     
